d1c8158f974bab984dc989eebd4728354309731c
Abhishek bhagat <abhishekbhagatraw@gmail.com>
2020-09-30 00:52:20 +0530

    [abhishek] to add person details

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,6 +1,16 @@
 package com.bridgelabz.problem;
-public class AddressBookMain {
-  public static void main(String[] args) {
-	   System.out.println("Welcome to Address Book Program");
-}
+import java.util.LinkedList;
+public class AddressBookMain{
+	private LinkedList<ContactPerson> contactPersonList;
+	public AddressBookMain(){
+		  contactPersonList=new LinkedList<>();
+	}
+	private void addContactPerson(String firstName,String lastName,String address,String city,String state,String zip,double phoneNumber,String email){
+	         ContactPerson contactPerson=new ContactPerson(firstName,lastName,address,city,state,zip,phoneNumber,email);
+	         contactPersonList.add(contactPerson);   
+	}
+        public static void main(String[] args) {
+	     AddressBookMain addressBookMain =new AddressBookMain();
+	     addressBookMain.addContactPerson("ABHISHEK","BHAGAT", "DHANBAD,JHARKHAND","KATRASGARH","JHARKHAND","828113",720156345,"abhishekbhagatrw@gmail.com");
+    }
 }
