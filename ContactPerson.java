633d7fabc5c7b7b6da4f84f6ba5d02cb138bc44f
Abhishek bhagat <abhishekbhagatraw@gmail.com>
2020-09-30 00:43:01 +0530

    [ahishek] to create a contact in address book with first name and last names,zip,city and email...

---
+++ ContactPerson.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.problem;
+public class ContactPerson {
+	  private String firstName;
+	  private String lastName;
+	  private String address;
+	  private String city;
+	  private String state;
+	  private String zip;
+	  private double phoneNumber;
+	  private String email;
+	  public ContactPerson(String firstName,String lastName,String address,String city,String state,String zip,Double phoneNumber,String email){
+		this.firstName=firstName;
+	        this.lastName=lastName;
+	        this.address=address;
+	        this.city=city;
+	        this.state=state;
+	        this.zip=zip;
+                this.phoneNumber=phoneNumber;
+	        this.email=email;
+	 }
+	 
+}
