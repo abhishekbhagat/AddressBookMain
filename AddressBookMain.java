5c4f9356a42684c4bd17955083b28e8ac075ac7f
Abhishek bhagat <abhishekbhagatraw@gmail.com>
2020-09-30 07:04:55 +0530

    [abhishek] to add a new contact

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -8,9 +8,27 @@ public class AddressBookMain{
 	private void addContactPerson(String firstName,String lastName,String address,String city,String state,String zip,double phoneNumber,String email){
 	         ContactPerson contactPerson=new ContactPerson(firstName,lastName,address,city,state,zip,phoneNumber,email);
 	         contactPersonList.add(contactPerson);   
-	}
+	  }
         public static void main(String[] args) {
+	     System.out.println("Welcome to Address Book Program");
 	     AddressBookMain addressBookMain =new AddressBookMain();
-	     addressBookMain.addContactPerson("ABHISHEK","BHAGAT", "DHANBAD,JHARKHAND","KATRASGARH","JHARKHAND","828113",7201563452,"abhishekbhagatrw@gmail.com");
-    }
-}
+	     Scanner sc =new Scanner(System.in);
+	     System.out.println("Enter the First Name ");
+	     String firstName=sc.next();
+	     System.out.println("Enter the last Name ");
+	     String lastName=sc.next();
+	     System.out.println("Enter the address ");
+	     String address=sc.next();
+	     System.out.println("Enter the  city ");
+	     String city=sc.next();
+	     System.out.println("Enter the state ");
+	     String state=sc.next();
+	     System.out.println("Enter the zip ");
+	     String zip=sc.next();
+	     System.out.println("Enter the phoneNumber ");
+	     double phoneNumber=sc.nextDouble();
+	     System.out.println("Enter the email ");
+	     String email=sc.next();
+	     addressBookMain.addContactPerson(firstName,lastName ,address,city,state,zip, phoneNumber,email);
+        }
+ }
