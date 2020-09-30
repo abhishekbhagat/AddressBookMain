0284be1861fd73384d503a35ef71ede4641b7213
Abhishek bhagat <abhishekbhagatraw@gmail.com>
2020-09-30 06:11:48 +0530

    [abhishek] to delete a person using person's name

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,4 +1,5 @@
 package com.bridgelabz.problem;
+import com.bridgelabz.problem.ContactPerson;
 import java.util.LinkedList;
 public class AddressBookMain{
 	private LinkedList<ContactPerson> contactPersonList;
@@ -11,13 +12,17 @@ public class AddressBookMain{
 	}
         private void editContactPersonName(String firstName,String lastName){
 		  for(ContactPerson contactPerson: contactPersonList) {
-			  if(contactPerson.getFirstName().equals(firstName)&&contactPerson.getLastName().equals(lastName))
-			  {
-				  
-			  }
-			  
+			  if(contactPerson.getFirstName().equals(firstName)&&contactPerson.getLastName().equals(lastName)){
+		          }
 		  }
-	  }
+	 }
+        private void deleteContactPerson(String firstName,String lastName){
+		  for(ContactPerson contactPerson:contactPersonList){
+			  if(contactPerson.getFirstName().equals(firstName)&&contactPerson.getLastName().equals(lastName)){
+				  contactPersonList.remove(contactPerson);
+			  }
+		  }
+	}
         public static void main(String[] args) {
 	     AddressBookMain addressBookMain =new AddressBookMain();
 	     addressBookMain.addContactPerson("ABHISHEK","BHAGAT", "DHANBAD,JHARKHAND","KATRASGARH","JHARKHAND","828113",7201563452,"abhishekbhagatrw@gmail.com");
